31199885ba3888cf06dce684bb3a6ff7b5569c9a
SubhikshaKarna <subhikshakarna@gmail.com>
2025-06-05 15:32:37 +0530

    Second commit~

--- obj/Debug/net8.0/ReqnrollProject1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReqnrollProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReqnrollProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+877b8c260f530787710e28c11964a80330a462f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReqnrollProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReqnrollProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
